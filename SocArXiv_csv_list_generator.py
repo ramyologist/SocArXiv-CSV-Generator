6943423f0cfcf8b60247365559dafc2a957d60a6
ramyologist <88153202+ramyologist@users.noreply.github.com>
2023-10-10 01:46:25 +0200

    Update SocArXiv_csv_list_generator.py

--- SocArXiv_csv_list_generator.py
+++ SocArXiv_csv_list_generator.py
@@ -10,10 +10,10 @@ st.write(
     This app allows you to generate links to SocArXiv preprints based on the selected subject.
     Simply choose a subject from the dropdown list and click on 'Create CSV File', 
     to generate a CSV file with a list containing titles and links to the preprints of the selected subject.
-    \n\n The script currently still has problems displaying the author names. The problem is because the API 
-    does not return the author names in a consistent format. Sometimes they are as a list of strings, sometimes 
-    as a single string separated by commas. The script cannot handle this inconsistency properly and leaves 
-    the author names empty.
+    \n\n The script currently has problems displaying the author names, because the API does not return the 
+    author names in a consistent format. Sometimes the names are a list of strings, sometimes they are as 
+    a single string separated by commas. The script cannot handle this inconsistency properly and leaves 
+    the author names blank.
     \n\n GitHub-Repo: https://github.com/ramyologist/SocArXiv-CSV-Generator
     \n Cheers, 
     \n ramyologist
